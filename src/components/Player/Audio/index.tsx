353bfad983214ef2d46a25464440c0270206d814
Sheraff <fpellet@ensc.fr>
2022-08-31 11:52:47 +0200

    better autoplay

--- src/components/Player/Audio/index.tsx
+++ src/components/Player/Audio/index.tsx
@@ -1,13 +1,15 @@
-import { ForwardedRef, forwardRef, memo } from "react"
+import { ForwardedRef, forwardRef, memo, useState } from "react"
 import { useCurrentTrack } from "components/AppContext/useCurrentTrack"
 
 const Audio = forwardRef(function Audio(_, ref: ForwardedRef<HTMLAudioElement>) {
 	const item = useCurrentTrack()
-	
+	const [autoPlay, setAutoPlay] = useState(false)
 	return (
 		<audio
 			ref={ref}
+			onPlay={autoPlay ? undefined : () => setAutoPlay(true)}
 			hidden
+			autoPlay={autoPlay}
 			playsInline
 			src={item?.id && `/api/file/${item.id}`}
 		/>
