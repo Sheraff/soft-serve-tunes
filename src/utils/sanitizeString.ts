ce394f9738aa45869fa51918384a85eabc77867d
Sheraff <fpellet@ensc.fr>
2022-09-13 00:28:48 +0200

    code comment

--- src/utils/sanitizeString.ts
+++ src/utils/sanitizeString.ts
@@ -1,6 +1,6 @@
 export default function sanitizeString(string: string) {
 	return string
-		.replace(/\$/g, 's') // exception because music titles are funky
+		.replace(/\$/g, 's') // Ke$ha
 		.replace(/[^\p{L}\p{N}\s'&]+/gu, ' ') // remove all non-alphanumeric characters (any language) except for "' &"
 		.replace(/\s+/g, ' ') // concatenate consecutive spaces
 		.trim()
