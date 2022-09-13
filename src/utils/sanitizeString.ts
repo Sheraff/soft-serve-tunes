ea4538de9ebd4ca407717bab680ec00e482d42e5
Sheraff <fpellet@ensc.fr>
2022-09-13 19:56:28 +0200

    acoustid add quotes

--- src/utils/sanitizeString.ts
+++ src/utils/sanitizeString.ts
@@ -1,6 +1,7 @@
 export default function sanitizeString(string: string) {
 	return string
 		.replace(/\$/g, 's') // Ke$ha
+		.replace(/’/g, "'")
 		.replace(/[^\p{L}\p{N}\s'&]+/gu, ' ') // remove all non-alphanumeric characters (any language) except for "' &"
 		.replace(/\s+/g, ' ') // concatenate consecutive spaces
 		.trim()
