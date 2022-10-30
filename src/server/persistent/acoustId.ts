3dd229587deed00f3585f86bdd78e6accf76c68c
Sheraff <fpellet@ensc.fr>
2022-10-30 21:10:59 +0100

    short tracks have little to no info from acoustid

--- src/server/persistent/acoustId.ts
+++ src/server/persistent/acoustId.ts
@@ -265,7 +265,7 @@ class AcoustId {
 		const mostConfidentRecordings = results
 			.filter(({score, recordings}) => (score > maxScore - 0.5) && recordings)
 			.flatMap(({score, recordings}) => recordings?.map((recording) => ({...recording, score}))) as (z.infer<typeof acoustIdRecordingSchema> & {score: number})[]
-		const sameDurationRecordings = metaDuration
+		const sameDurationRecordings = (metaDuration && metaDuration > 20) // short duration tracks usually don't have a lot of data from acoustid
 			? mostConfidentRecordings.filter(({score, title, duration: d}) => {
 				if (!title || !d) return false
 				const delta = Math.abs(metaDuration - d)
@@ -276,7 +276,7 @@ class AcoustId {
 				if (score > 0.999 && delta < 20) return true
 				return false
 			})
-			: mostConfidentRecordings.filter(({title}) => title)
+			: mostConfidentRecordings.filter(({title, score}) => title && score > 0.9)
 		if (sameDurationRecordings.length === 0) {
 			log("warn", "404", "acoustid", `Musicbrainz fingerprint matches don't match file duration: ${metaDuration} vs [${mostConfidentRecordings.map(({duration}) => duration).join(', ')}]`)
 			console.log(results)
