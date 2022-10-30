fa7d0f0d5fa3277af0545dbe22c5b2ec2c251bda
Sheraff <fpellet@ensc.fr>
2022-10-30 21:09:27 +0100

    increase tolerance in `similarStrings`

--- src/utils/similarStrings.ts
+++ src/utils/similarStrings.ts
@@ -14,5 +14,5 @@ export default function similarStrings(a: string, b: string) {
 	const distance = damLev(_a, _b)
 	const refLength = Math.max(_a.length, _b.length)
 	const ratio = distance / refLength
-	return ratio < 0.07 || (ratio < 0.5 && distance < 4)
+	return ratio < 0.07 || (ratio < 0.5 && distance < 4) || (ratio < 0.3 && distance < 5)
 }
