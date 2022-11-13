4257553aad5f704bfd757485a23ef1a213231782
Sheraff <fpellet@ensc.fr>
2022-11-13 17:26:57 +0100

    TrackList: "draggable" option

--- src/components/TrackList/index.tsx
+++ src/components/TrackList/index.tsx
@@ -4,15 +4,17 @@ import useIndexedTRcpQuery from "client/db/useIndexedTRcpQuery"
 import { inferQueryOutput } from "utils/trpc"
 import { playlist, useShowHome } from "components/AppContext"
 import styles from "./index.module.css"
-import useSlideTrack, { Callbacks } from "./useSlideTrack"
+import useSlideTrack, { Callbacks as SlideCallbacks } from "./useSlideTrack"
 import FavoriteIcon from "icons/favorite.svg"
 import PlaylistNextIcon from "icons/playlist_play.svg"
 import PlaylistAddIcon from "icons/playlist_add.svg"
 import PlayIcon from "icons/play_arrow.svg"
+import DragIcon from "icons/drag_indicator.svg"
 import { useSetAtom } from "jotai"
 import { trpc } from "utils/trpc"
+import useDragTrack, { Callbacks as DragCallbacks } from "./useDragTrack"
 
-const initialCallbacks = {
+const initialSlideCallbacks = {
 	onLike: () => {},
 	onAdd: () => {},
 	onNext: () => {},
@@ -24,12 +26,14 @@ function TrackItem({
 	current,
 	onClick,
 	onSelect,
+	draggable,
 }: {
 	track: inferQueryOutput<"track.searchable">[number]
 	enableSiblings?: () => void
 	current?: boolean
 	onClick?: (id:string, name:string) => void
 	onSelect?: (track: inferQueryOutput<"track.miniature">) => void
+	draggable?: boolean
 }) {
 	const item = useRef<HTMLDivElement>(null)
 	const {data} = useIndexedTRcpQuery(["track.miniature", {id: track.id}])
@@ -57,7 +61,7 @@ function TrackItem({
 	const isEmpty = !data?.cover
 
 	const {mutate: likeMutation} = trpc.useMutation(["track.like"])
-	const callbacks = useRef<Callbacks>(initialCallbacks)
+	const callbacks = useRef<SlideCallbacks>(initialSlideCallbacks)
 	callbacks.current.onLike = () => {
 		likeMutation({
 			id: track.id,
@@ -76,7 +80,8 @@ function TrackItem({
 
 	return (
 		<div ref={item} className={classNames(styles.wrapper, {
-			[styles.liked as string]: data?.userData?.favorite
+			[styles.liked as string]: data?.userData?.favorite,
+			[styles.draggable as string]: draggable,
 		})}>
 			<button
 				className={classNames(styles.button, {
@@ -96,6 +101,9 @@ function TrackItem({
 					}
 				}}
 			>
+				{draggable && (
+					<DragIcon className={styles.handle} data-handle/>
+				)}
 				{!isEmpty && (
 					<div className={styles.img}>
 						<img
@@ -128,22 +136,37 @@ function TrackItem({
 	)
 }
 
+const initialDragCallbacks = {
+	onDrop: () => {}
+}
+
 export default function TrackList({
 	tracks,
 	current,
 	onClick,
 	onSelect,
+	orderable,
+	onReorder,
 }: {
 	tracks: inferQueryOutput<"track.searchable">
 	current?: string
 	onClick?: Parameters<typeof TrackItem>[0]["onClick"]
 	onSelect?: (track: Exclude<inferQueryOutput<"track.miniature">, null>) => void
+	orderable?: boolean
+	onReorder?: (from: number, to: number) => void
 }) {
 	const [enableUpTo, setEnableUpTo] = useState(12)
+	const ref = useRef<HTMLUListElement>(null)
+	const callbacks = useRef<DragCallbacks>(initialDragCallbacks)
+	callbacks.current.onDrop = (from, to) => {
+		onReorder?.(from, to)
+		console.log('reorder tracks', from, to)
+	}
+	useDragTrack(ref, !!orderable, callbacks)
 	return (
-		<ul className={styles.main}>
+		<ul className={styles.main} ref={orderable ? ref : undefined}>
 			{tracks.map((track, i) => (
-				<li className={styles.item} key={track.id}>
+				<li className={styles.item} key={track.id} data-index={i}>
 					{i <= enableUpTo && (
 						<TrackItem
 							track={track}
@@ -151,6 +174,7 @@ export default function TrackList({
 							current={current === track.id}
 							onClick={onClick}
 							onSelect={onSelect}
+							draggable={orderable}
 						/>
 					)}
 				</li>
---
+++ src/components/TrackList/useDragTrack.tsx
@@ -0,0 +1,163 @@
+import { RefObject, useEffect } from "react"
+import styles from "./index.module.css"
+
+function getTouchFromId(list: TouchList, id: number) {
+	for (let i = 0; i < list.length; i++) {
+		const item = list.item(i)
+		if(item?.identifier === id)
+			return item
+	}
+}
+
+function scrollParent(node: HTMLElement): HTMLElement {
+	if (node.scrollHeight > node.clientHeight) {
+		return node
+	} else if (node.parentElement === document.documentElement) {
+		return node.parentElement
+	}
+	return scrollParent(node.parentElement!)
+}
+
+function iterateSiblings(
+	node: HTMLElement,
+	offset: number,
+	callback: (node: HTMLElement, inRange: boolean) => void
+) {
+	const referenceIndex = Number(node.dataset.index)
+	const extremumIndex = referenceIndex + offset
+	const firstIndex = Math.min(referenceIndex, extremumIndex)
+	const lastIndex = Math.max(referenceIndex, extremumIndex)
+	function nextSibling(sibling: HTMLElement, index: number) {
+		if (sibling !== node) {
+			const isInRange = index >= firstIndex && index <= lastIndex
+			callback(sibling, isInRange)
+		}
+		if (sibling.nextElementSibling) {
+			nextSibling(sibling.nextElementSibling as HTMLElement, index + 1)
+		}
+	}
+	nextSibling(node.parentElement!.firstElementChild as HTMLElement, 0)
+}
+
+export type Callbacks = {
+	onDrop: (from: number, to: number) => void
+}
+
+export default function useDragTrack<T extends boolean>(
+	ref: RefObject<HTMLElement>,
+	enabled: T,
+	callbacks: T extends true ? {current: Callbacks} : {current?: Partial<Callbacks>}
+) {
+	useEffect(() => {
+		if (!enabled) return
+		const element = ref.current
+		if (!element) return
+		const controller = new AbortController()
+		const {signal} = controller
+
+		let isDragging = false
+		let uxController: AbortController | null = null
+		let rafId: number | null = null
+
+		function start(touch: Touch | null, item: HTMLElement | null) {
+			if (!touch || !item) return
+			const controller = new AbortController()
+			const {signal} = controller
+			uxController = controller
+			isDragging = true
+			let itemsOffset = 0
+			let scrollSpeed = 0
+			let dy = 0
+
+			const scrollContainer = scrollParent(item)
+			const initialScroll = scrollContainer.scrollTop
+			const itemHeight = item.offsetHeight
+			const totalSiblings = item.parentElement!.childElementCount
+			const itemIndex = Number(item.dataset.index)
+
+			item.classList.add(styles.drag as string)
+
+			window.addEventListener('contextmenu', (event) => {
+				event.preventDefault()
+			}, {signal})
+
+			function scrollFrame(lastTime?: number) {
+				rafId = requestAnimationFrame((time) => {
+					scrollFrame(time)
+
+					// scroll parent
+					if (lastTime) {
+						scrollContainer.scrollTop += scrollSpeed * (time - lastTime) / 1
+					}
+
+					// compute distance traveled
+					const totalOffset = dy + scrollContainer.scrollTop - initialScroll
+					item!.style.setProperty('--y', `${totalOffset}px`)
+					
+					// move siblings
+					const currentItemsOffset = totalOffset > 0
+						? Math.floor(totalOffset / itemHeight + 0.5)
+						: Math.ceil(totalOffset / itemHeight - 0.5)
+					const clampedItemsOffset = Math.min(totalSiblings - itemIndex - 1, Math.max(-itemIndex, currentItemsOffset))
+					if (itemsOffset !== clampedItemsOffset) {
+						itemsOffset = clampedItemsOffset
+						iterateSiblings(item!, itemsOffset, (sibling, inRange) => {
+							sibling.classList.toggle(styles.slide as string, inRange)
+						})
+						item!.style.setProperty('--bg-y', `${itemsOffset}`)
+					}
+				})
+			}
+			scrollFrame()
+
+			window.addEventListener('touchmove', (event) => {
+				const match = getTouchFromId(event.changedTouches, touch.identifier)
+				if (!match) return
+
+				// move item
+				dy = match.clientY - touch.clientY
+
+				// scroll parent
+				scrollSpeed = match.clientY < innerHeight / 3
+					? (match.clientY - innerHeight / 3) / (innerHeight / 3)
+					: match.clientY > innerHeight * 3 / 4
+						? (match.clientY - innerHeight * 3 / 4) / (innerHeight / 4)
+						: 0
+			}, {signal, passive: true})
+
+			window.addEventListener('touchend', (event) => {
+				const match = getTouchFromId(event.changedTouches, touch.identifier)
+				if (!match) return
+				item.classList.remove(styles.drag as string)
+				item.style.removeProperty('--y')
+				item.style.removeProperty('--bg-y')
+				controller.abort()
+				uxController = null
+				isDragging = false
+				if (rafId) cancelAnimationFrame(rafId)
+				Array.from(item.parentElement!.children).forEach((node) => node.classList.remove(styles.slide as string))
+				callbacks.current!.onDrop!(itemIndex, itemIndex + itemsOffset)
+			}, {signal, passive: false})
+		}
+
+		element.addEventListener('touchstart', (event) => {
+			const target = event.target as HTMLElement
+			if (!target.dataset.handle) return
+			
+			if (!isDragging) {
+				event.preventDefault()
+				event.stopPropagation()
+				const touch = event.changedTouches.item(0)
+				const item = target.closest(`.${styles.item}`) as HTMLElement | null
+				start(touch, item)
+			}
+		}, {signal, capture: true})
+
+		return () => {
+			controller.abort()
+			if (uxController) uxController.abort()
+			if(rafId) cancelAnimationFrame(rafId)
+		}
+	}, [ref, enabled, callbacks])
+
+}
