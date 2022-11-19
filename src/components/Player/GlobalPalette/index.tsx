3b94028788ba742a49d43deee49cd4893936a4d8
Sheraff <fpellet@ensc.fr>
2022-11-19 20:05:43 +0100

    a little more stable PWA menubar color

--- src/components/Player/GlobalPalette/index.tsx
+++ src/components/Player/GlobalPalette/index.tsx
@@ -1,12 +1,13 @@
 import { useCurrentTrackDetails } from "components/AppContext/useCurrentTrack"
 import Palette from "components/Palette"
+import { useMemo } from "react"
 
 export default function GlobalPalette() {
 	const data = useCurrentTrackDetails()
 
-	return (
+	return useMemo(() => (
 		<Palette
 			palette={data?.cover ? JSON.parse(data.cover.palette) : undefined}
 		/>
-	)
+	), [data?.cover])
 }
