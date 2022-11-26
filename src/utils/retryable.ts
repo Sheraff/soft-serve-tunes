b22b55b739011000d3c75631b0a848882663cb20
Sheraff <fpellet@ensc.fr>
2022-11-26 21:46:24 +0100

    retryable still logs errors it catches

--- src/utils/retryable.ts
+++ src/utils/retryable.ts
@@ -4,6 +4,9 @@ export default async function retryable<T>(callback: () => (Promise<T> | T), tri
 		return result
 	} catch (e) {
 		if (tries < 5) {
+			const code = typeof e === 'object' ? ` code:${e.code}` : ''
+			const keys = typeof e === 'object' ? ` keys[${Object.keys(e).join(',')}]` : ''
+			console.warn(new Error(`Error in retryable${code}${keys}, will retry #${tries}`, {cause: e}))
 			await new Promise(resolve => setTimeout(resolve, Math.random() * 10 + 1000 * 2**tries))
 			const result = await retryable(callback, tries + 1)
 			return result
