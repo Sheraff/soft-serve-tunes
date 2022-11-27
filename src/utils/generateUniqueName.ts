55b2f331a47fbddca65cc4be7689f490bc90ca7d
Sheraff <fpellet@ensc.fr>
2022-11-27 23:34:01 +0100

    generating unique name takes pre-existing numbering into account

--- src/utils/generateUniqueName.ts
+++ src/utils/generateUniqueName.ts
@@ -1,8 +1,11 @@
 export default function generateUniqueName(name: string, list: {name: string}[]) {
-	let appendCount = 1
+	const existingNumberRegex = /\s#([0-9]+)$/
+	const match = name.match(existingNumberRegex)
+	let appendCount = match ? Number(match[1]) : 1
+	const baseName = match ? name.replace(existingNumberRegex, '') : name
 	let uniqueName: string
 	do {
-		uniqueName = appendCount > 1 ? `${name} #${appendCount}` : name
+		uniqueName = appendCount > 1 ? `${baseName} #${appendCount}` : baseName
 		appendCount += 1
 	} while (list.some(({name}) => name === uniqueName))
 	return uniqueName
