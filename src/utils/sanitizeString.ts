73f256fc8ac5bd6dd90e3dedf09c9ce13b17e22a
Sheraff <fpellet@ensc.fr>
2023-01-10 21:41:14 +0100

    fix string sanitizer for album "Hawaï"

--- src/utils/sanitizeString.ts
+++ src/utils/sanitizeString.ts
@@ -3,7 +3,7 @@ import toTitleCase from "titlecase"
 export default function sanitizeString(string: string) {
 	return string
 		.replace(/\$/g, "s") // Ke$ha
-		.replace(/ï/g, "ii") // Hawaï
+		.replace(/ï/g, "i") // Hawaï
 		.replace(/’/g, "'")
 		.replace(/[^\p{L}\p{N}\s'&]+/gu, " ") // remove all non-alphanumeric characters (any language) except for "' &"
 		.replace(/\s+/g, " ") // concatenate consecutive spaces
