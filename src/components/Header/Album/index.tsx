c036b8641d78ce668155419cf161bef0ac093f97
Sheraff <fpellet@ensc.fr>
2023-01-13 17:22:09 +0100

    unify album & artist panels

--- src/components/Header/Album/index.tsx
+++ src/components/Header/Album/index.tsx
@@ -1,19 +1,16 @@
-import { type CSSProperties, type ForwardedRef, forwardRef, Fragment, startTransition, useDeferredValue, useEffect, useImperativeHandle, useMemo, useRef, useState } from "react"
+import { type ForwardedRef, forwardRef, useDeferredValue, useMemo, Fragment } from "react"
 import pluralize from "utils/pluralize"
-import { albumView, artistView, useShowHome } from "components/AppContext"
-import PlayIcon from "icons/play_arrow.svg"
+import { albumView, artistView } from "components/AppContext"
 import styles from "./index.module.css"
-import classNames from "classnames"
 import TrackList from "components/TrackList"
-import { paletteToCSSProperties } from "components/Palette"
 import SectionTitle from "atoms/SectionTitle"
-import Head from "next/head"
 import { trpc } from "utils/trpc"
 import { useMakePlaylist } from "client/db/useMakePlaylist"
 import { useQueryClient } from "@tanstack/react-query"
+import Panel from "../Panel"
 
 export default forwardRef(function AlbumView({
-	open: _open,
+	open,
 	id,
 	z,
 }: {
@@ -21,37 +18,15 @@ export default forwardRef(function AlbumView({
 	id: string
 	z: number
 }, ref: ForwardedRef<HTMLDivElement>) {
-	const open = useDeferredValue(_open)
 	const album = albumView.useValue()
-	const enabled = Boolean(id && album.open)
 
+	const enabled = Boolean(id && album.open)
 	const {data} = trpc.album.get.useQuery({id}, {
 		enabled,
 		keepPreviousData: true,
 	})
 
-	const makePlaylist = useMakePlaylist()
-
-	const showHome = useShowHome()
 	const queryClient = useQueryClient()
-
-	const [seeBio, setSeeBio] = useState<boolean | null>(false) // null means "no need for toggle, small enough"
-	const bio = useRef<HTMLDivElement>(null)
-	useEffect(() => {
-		const element = bio.current
-		if (!element) return
-		const observer = new ResizeObserver(([entry]) => {
-			if (entry) {
-				const parent = entry.target.parentElement as HTMLDivElement
-				if (parent.offsetHeight >= Math.floor(entry.contentRect.height)) {
-					setSeeBio(null)
-				}
-			}
-		})
-		observer.observe(element)
-		return () => observer.disconnect()
-	}, [data?.audiodb?.strDescriptionEN])
-
 	const infos = []
 	const date = data?.spotify?.releaseDate || data?.audiodb?.intYearReleased || data?.lastfm?.releasedate
 	if (date) {
@@ -78,111 +53,38 @@ export default forwardRef(function AlbumView({
 		infos.push(`${data?._count.tracks} track${pluralize(data?._count.tracks)}`)
 	}
 
-	const palette = paletteToCSSProperties(data?.cover?.palette)
-
-	const main = useRef<HTMLDivElement>(null)
-	useImperativeHandle(ref, () => main.current as HTMLDivElement)
-
-	// synchronously compute initial position if an `artist.rect` emitter has been set
-	const initialPositionRef = useRef<CSSProperties | null>(null)
-	const initialImageSrc = useRef<string | null>(null)
-	if (open && !initialPositionRef.current && album.rect) {
-		initialPositionRef.current = {
-			"--top": `${album.rect.top}px`,
-			"--left": `${album.rect.left}px`,
-			"--scale": `${album.rect.width / innerWidth}`,
-		} as CSSProperties
-		initialImageSrc.current = album.rect.src || null
+	const makePlaylist = useMakePlaylist()
+	const onClickPlay = () => {
+		const playlistName = !data
+			? "New Playlist"
+			: !data.artist
+			? data.name
+			: `${data.name} by ${data.artist.name}`
+		makePlaylist({type: "album", id}, playlistName)
 	}
 
 	const tracks = useDeferredValue(data?.tracks)
+	const children = useMemo(() => tracks && Boolean(tracks.length) && (
+		<>
+			<SectionTitle className={styles.sectionTitle}>Tracks</SectionTitle>
+			<TrackList tracks={tracks} />
+		</>
+	), [tracks])
 
 	return (
-		<div
-			className={styles.main}
-			data-open={open}
-			data-bubble={initialPositionRef.current !== null}
-			ref={main}
-			style={{
-				"--z": z,
-				...palette,
-				...(initialPositionRef.current || {}),
-			} as CSSProperties}
+		<Panel
+			ref={ref}
+			open={open}
+			z={z}
+			view={album}
+			description={data?.audiodb?.strDescriptionEN}
+			cover={data?.cover}
+			infos={infos}
+			title={data?.name}
+			onClickPlay={onClickPlay}
+			animationName={styles["bubble-open"]}
 		>
-			{palette && album.open && (
-				<Head>
-					<meta name="theme-color" content={palette["--palette-bg-main"]} />
-				</Head>
-			)}
-			<img
-				className={classNames(styles.img, styles.preview)}
-				src={initialImageSrc.current || ""}
-				alt=""
-			/>
-			<img
-				className={styles.img}
-				src={data?.cover ? `/api/cover/${data?.cover.id}` : ""}
-				alt=""
-				decoding="async"
-			/>
-			<div className={styles.head}>
-				<SectionTitle className={styles.sectionTitle}>{data?.name}</SectionTitle>
-				<p className={styles.info}>
-					{infos.map((info, i) => (
-						<Fragment key={i}>
-							{i > 0 ? " · " : ""}
-							{info}
-						</Fragment>
-					))}
-				</p>
-				{data?.audiodb?.strDescriptionEN && (
-					<div
-						className={classNames(styles.bio, {[styles.seeBio]: seeBio !== false})}
-						onClick={seeBio !== null ? () => {
-							navigator.vibrate(1)
-							setSeeBio(!seeBio)
-						} : undefined}
-					>
-						<div className={styles.bioText}>
-							<div ref={bio}>
-								{data?.audiodb?.strDescriptionEN}
-							</div>
-						</div>
-						{seeBio !== null && (
-							<button
-								className={styles.toggle}
-								type="button"
-							>
-								{seeBio ? "...Less" : "More..."}
-							</button>
-						)}
-					</div>
-				)}
-				<button
-					className={styles.play}
-					type="button"
-					onClick={() => {
-						const playlistName = !data
-							? "New Playlist"
-							: !data.artist
-							? data.name
-							: `${data.name} by ${data.artist.name}`
-						navigator.vibrate(1)
-						startTransition(() => {
-							makePlaylist({type: "album", id}, playlistName)
-							showHome("home")
-						})
-					}}
-				>
-					<PlayIcon />
-				</button>
-			</div>
-			{useMemo(() => tracks && Boolean(tracks.length) && (
-				<div className={styles.section}>
-					<SectionTitle className={styles.sectionTitle}>Tracks</SectionTitle>
-					<TrackList tracks={tracks} />
-				</div>
-			), [tracks])}
-		</div>
+			{children}
+		</Panel>
 	)
 })
---
+++ src/components/Header/Panel/index.tsx
@@ -0,0 +1,167 @@
+import { type ForwardedRef, forwardRef, useDeferredValue, useState, useRef, useEffect, type ReactNode, useImperativeHandle, CSSProperties, startTransition, Children, Fragment } from "react"
+import { useShowHome } from "components/AppContext"
+import PlayIcon from "icons/play_arrow.svg"
+import { type Prisma } from "@prisma/client"
+import { paletteToCSSProperties } from "components/Palette"
+import Head from "next/head"
+import classNames from "classnames"
+import styles from "./index.module.css"
+import SectionTitle from "atoms/SectionTitle"
+
+export default forwardRef(function PanelView({
+	open: _open,
+	z,
+	view,
+	description,
+	cover,
+	infos,
+	title,
+	onClickPlay,
+	children,
+	animationName
+}: {
+	open: boolean
+	z: number
+	view: {
+		open: boolean
+		rect?: {
+			top: number,
+			left: number,
+			width: number,
+			src?: string,
+		}
+	}
+	description: string | null | undefined
+	cover: {
+		id: string
+		palette: Prisma.JsonValue
+	} | null | undefined
+	infos: ReactNode[]
+	title: string | undefined
+	onClickPlay: () => void
+	children: ReactNode
+	animationName: string
+}, ref: ForwardedRef<HTMLDivElement>) {
+	const open = useDeferredValue(_open)
+
+	const showHome = useShowHome()
+
+	const [seeBio, setSeeBio] = useState<boolean | null>(false) // null means "no need for toggle, small enough"
+	const bio = useRef<HTMLDivElement>(null)
+	useEffect(() => {
+		const element = bio.current
+		if (!element) return
+		const observer = new ResizeObserver(([entry]) => {
+			if (entry) {
+				const parent = entry.target.parentElement as HTMLDivElement
+				if (parent.offsetHeight >= Math.floor(entry.contentRect.height)) {
+					setSeeBio(null)
+				}
+			}
+		})
+		observer.observe(element)
+		return () => observer.disconnect()
+	}, [description])
+
+	const palette = paletteToCSSProperties(cover?.palette)
+
+	const main = useRef<HTMLDivElement>(null)
+	useImperativeHandle(ref, () => main.current as HTMLDivElement)
+
+	// synchronously compute initial position if an `artist.rect` emitter has been set
+	const initialPositionRef = useRef<CSSProperties | null>(null)
+	const initialImageSrc = useRef<string | null>(null)
+	if (open && !initialPositionRef.current && view.rect) {
+		initialPositionRef.current = {
+			"--top": `${view.rect.top}px`,
+			"--left": `${view.rect.left}px`,
+			"--scale": `${view.rect.width / innerWidth}`,
+			"--end": `${Math.hypot(innerWidth, innerHeight)}px`,
+		} as CSSProperties
+		initialImageSrc.current = view.rect.src || null
+	}
+
+	return (
+		<div
+			className={styles.main}
+			data-open={open}
+			data-bubble={initialPositionRef.current !== null}
+			ref={main}
+			style={{
+				"--z": z,
+				"--bubble-open": animationName,
+				...palette,
+				...(initialPositionRef.current || {}),
+			} as CSSProperties}
+		>
+			{palette && view.open && (
+				<Head>
+					<meta name="theme-color" content={palette["--palette-bg-main"]} />
+				</Head>
+			)}
+			<img
+				className={classNames(styles.img, styles.preview)}
+				src={initialImageSrc.current || ""}
+				alt=""
+			/>
+			<img
+				className={styles.img}
+				src={cover ? `/api/cover/${cover?.id}` : ""}
+				alt=""
+				decoding="async"
+			/>
+			<div className={styles.head}>
+				<SectionTitle className={styles.sectionTitle}>{title}</SectionTitle>
+				<p className={styles.info}>
+					{infos.map((info, i) => (
+						<Fragment key={i}>
+							{i > 0 ? " · " : ""}
+							{info}
+						</Fragment>
+					))}
+				</p>
+				{description && (
+					<div
+						className={classNames(styles.bio, {[styles.seeBio]: seeBio !== false})}
+						onClick={seeBio !== null ? () => {
+							navigator.vibrate(1)
+							setSeeBio(!seeBio)
+						} : undefined}
+					>
+						<div className={styles.bioText}>
+							<div ref={bio}>
+								{description}
+							</div>
+						</div>
+						{seeBio !== null && (
+							<button
+								className={styles.toggle}
+								type="button"
+							>
+								{seeBio ? "...Less" : "More..."}
+							</button>
+						)}
+					</div>
+				)}
+				<button
+					className={styles.play}
+					type="button"
+					onClick={() => {
+						navigator.vibrate(1)
+						startTransition(() => {
+							onClickPlay()
+							showHome("home")
+						})
+					}}
+				>
+					<PlayIcon />
+				</button>
+			</div>
+			{Children.map(children, child => (
+				<div className={styles.section}>
+					{child}
+				</div>
+			))}
+		</div>
+	)
+})
