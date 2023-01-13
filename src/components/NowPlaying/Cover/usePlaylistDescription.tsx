d5ec20bb521dcb4d1caeb69646a990d7d0a30118
Sheraff <fpellet@ensc.fr>
2023-01-13 21:46:12 +0100

    playlist panel pretty much ok

---
+++ src/components/NowPlaying/Cover/usePlaylistDescription.tsx
@@ -0,0 +1,44 @@
+import { useQueryClient } from "@tanstack/react-query"
+import descriptionFromPlaylistCredits from "client/db/useMakePlaylist/descriptionFromPlaylistCredits"
+import { artistView } from "components/AppContext"
+import { Fragment, useMemo } from "react"
+
+export default function usePlaylistDescription({
+	artistData,
+	length,
+}: {
+	artistData: {
+		name: string
+		id: string
+	}[]
+	length?: number
+}) {
+	const queryClient = useQueryClient()
+	const description = useMemo(() => {
+		const string = descriptionFromPlaylistCredits(artistData, length, true)
+		const parts = string.split("{{name}}")
+		return parts.flatMap((part, i) => [
+			<Fragment key={i}>{part}</Fragment>,
+			i === parts.length - 1
+				? null
+				: (
+					<button
+						key={artistData[i]!.id}
+						type="button"
+						onClick={() => {
+							navigator.vibrate(1)
+							artistView.setState({
+								id: artistData[i]!.id,
+								name: artistData[i]!.name,
+								open: true,
+							}, queryClient)
+						}}
+					>
+						{artistData[i]!.name}
+					</button>
+				)
+		])
+	}, [artistData, length, queryClient])
+	
+	return description
+}
