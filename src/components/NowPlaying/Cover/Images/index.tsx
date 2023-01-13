f7958b976c1f3196fdcb0be80b614931f50052e0
Sheraff <fpellet@ensc.fr>
2023-01-13 17:38:53 +0100

    extract playlist cover images in their own component

---
+++ src/components/NowPlaying/Cover/Images/index.tsx
@@ -0,0 +1,35 @@
+import classNames from "classnames"
+import { type RouterOutputs, trpc } from "utils/trpc"
+import styles from "./index.module.css"
+
+type AlbumMiniature = Exclude<RouterOutputs["album"]["miniature"], null>
+
+export default function Images({
+	albums = [],
+}: {
+	albums?: {id: string}[]
+}) {
+	const {albumData = []} = trpc
+		.useQueries((t) => albums.map(({id}) => t.album.miniature({id})))
+		.reduce<{
+			albumData: AlbumMiniature[]
+		}>((acc, {data}) => {
+			if (data?.cover) {
+				acc.albumData.push(data)
+			}
+			return acc
+		}, {albumData: []})
+
+	return (
+		<div className={classNames(styles.main, styles[`count-${albumData.length}` as keyof typeof styles])}>
+			{albumData.map((album) => (
+				<img
+					key={album.id}
+					className={styles.img}
+					src={`/api/cover/${album.cover!.id}`}
+					alt=""
+				/>
+			))}
+		</div>
+	)
+}
