767af6a0f6a233e0ab6423c0642428f229632780
Sheraff <fpellet@ensc.fr>
2023-02-04 09:44:44 +0100

    remove "rap" segment of speechiness spectrum

--- src/components/Suggestions/ByTrait/dictionaries.ts
+++ src/components/Suggestions/ByTrait/dictionaries.ts
@@ -69,15 +69,11 @@ export const FEATURES = {
 			qualifier: "melodic",
 			listOrder: 2,
 		},
-		"0.3": {
+		"0.4": {
 			qualifier: "song",
 			listOrder: 4,
 		},
-		"0.6": {
-			qualifier: "rap",
-			listOrder: 4,
-		},
-		"1": {
+		"0.8": {
 			qualifier: "prose",
 			listOrder: 4,
 		},
@@ -86,87 +82,87 @@ export const FEATURES = {
 
 export const COMBINED_FEATURES = [
 	{
-		traits: {danceability: "0"},
+		traits: { danceability: "0" },
 		description: "Lackadaisical {{type}}",
 	},
 	{
-		traits: {danceability: "1"},
+		traits: { danceability: "1" },
 		description: "{{Type}} with infectious beats",
 	},
 	{
-		traits: {energy: "0"},
+		traits: { energy: "0" },
 		description: "{{Type}} to relax to",
 	},
 	{
-		traits: {energy: "1"},
+		traits: { energy: "1" },
 		description: "High-octane {{type}}",
 	},
 	{
-		traits: {acousticness: "0"},
+		traits: { acousticness: "0" },
 		description: "Electric {{type}}",
 	},
 	{
-		traits: {acousticness: "1"},
+		traits: { acousticness: "1" },
 		description: "Acoustic {{type}}",
 	},
 	{
-		traits: {instrumentalness: "0"},
+		traits: { instrumentalness: "0" },
 		description: "Minimalist {{type}}",
 	},
 	{
-		traits: {instrumentalness: "0.5"},
+		traits: { instrumentalness: "0.5" },
 		description: "Musical {{type}}",
 	},
 	{
-		traits: {instrumentalness: "1"},
+		traits: { instrumentalness: "1" },
 		description: "Atmospheric {{type}}",
 	},
 	{
-		traits: {liveness: "0"},
+		traits: { liveness: "0" },
 		description: "{{Type}} recorded in a studio",
 	},
 	{
-		traits: {liveness: "1"},
+		traits: { liveness: "1" },
 		description: "{{Type}} performed live",
 	},
 	{
-		traits: {valence: "0"},
+		traits: { valence: "0" },
 		description: "Melancholic {{type}}",
 	},
 	{
-		traits: {valence: "1"},
+		traits: { valence: "1" },
 		description: "Cheerful {{type}}",
 	},
 	{
-		traits: {speechiness: "0"},
+		traits: { speechiness: "0" },
 		description: "Melodic {{type}}",
 	},
 	{
-		traits: {speechiness: "0.3"},
+		traits: { speechiness: "0.3" },
 		description: "Mid speech {{type}}",
 	},
 	{
-		traits: {speechiness: "0.7"},
+		traits: { speechiness: "0.7" },
 		description: "Mid speech {{type}}",
 	},
 	{
-		traits: {speechiness: "1"},
+		traits: { speechiness: "1" },
 		description: "{{Type}} with the best flow",
 	},
 	{
-		traits: {danceability: "1", energy: "1"},
+		traits: { danceability: "1", energy: "1" },
 		description: "{{Type}} for clubbing",
 	},
 	{
-		traits: {acousticness: "0", energy: "1"},
+		traits: { acousticness: "0", energy: "1" },
 		description: "{{Type}} for headbanging",
 	},
 	{
-		traits: {danceability: "1", energy: "1", acousticness: "0"},
+		traits: { danceability: "1", energy: "1", acousticness: "0" },
 		description: "{{Type}} for a rave",
 	},
 	{
-		traits: {danceability: "1", speechiness: "1"},
+		traits: { danceability: "1", speechiness: "1" },
 		description: "Grooviest prose",
 	},
 	// {
@@ -178,103 +174,103 @@ export const COMBINED_FEATURES = [
 	// 	description: "{{Type}} for karaoke",
 	// },
 	{
-		traits: {instrumentalness: "0", danceability: "1", energy: "1", valence: "1"},
+		traits: { instrumentalness: "0", danceability: "1", energy: "1", valence: "1" },
 		description: "{{Type}} for a party",
 	},
 	{
-		traits: {energy: "1", acousticness: "0", valence: "1"},
+		traits: { energy: "1", acousticness: "0", valence: "1" },
 		ignore: ["danceability", "liveness"],
 		description: "High-energy {{danceability}} {{liveness}} {{type}}",
 	},
 	{
-		traits: {energy: "1", acousticness: "0", valence: "0"},
+		traits: { energy: "1", acousticness: "0", valence: "0" },
 		ignore: ["danceability", "liveness"],
 		description: "{{Danceability}} emo {{liveness}} {{type}}",
 	},
 	{
-		traits: {energy: "0", acousticness: "1", valence: "0"},
+		traits: { energy: "0", acousticness: "1", valence: "0" },
 		ignore: ["danceability", "liveness", "instrumentalness", "speechiness"],
 		description: "{{Danceability}} {{speechiness}} {{instrumentalness}} {{liveness}} ballads",
 	},
 	// chatGPT generated
 	{
-		traits: {energy: "1", valence: "1", acousticness: "0"},
+		traits: { energy: "1", valence: "1", acousticness: "0" },
 		description: "Energetic {{type}} for getting pumped up",
 	},
 	{
-		traits: {energy: "0", acousticness: "1", valence: "0", instrumentalness: "1"},
+		traits: { energy: "0", acousticness: "1", valence: "0", instrumentalness: "1" },
 		description: "Soothing {{type}} for relaxation",
 	},
 	{
-		traits: {energy: "1", danceability: "1", valence: "1", speechiness: "0"},
+		traits: { energy: "1", danceability: "1", valence: "1", speechiness: "0" },
 		description: "Upbeat {{type}} for a road trip",
 	},
 	{
-		traits: {energy: "0", acousticness: "1", valence: "0", instrumentalness: "0"},
+		traits: { energy: "0", acousticness: "1", valence: "0", instrumentalness: "0" },
 		description: "Introspective {{type}} for a rainy day",
 	},
 	{
-		traits: {energy: "1", danceability: "1", valence: "1", liveness: "1"},
+		traits: { energy: "1", danceability: "1", valence: "1", liveness: "1" },
 		description: "Fiery {{type}} for a dance-off",
 	},
 	{
-		traits: {energy: "0", acousticness: "0", valence: "1", instrumentalness: "0"},
+		traits: { energy: "0", acousticness: "0", valence: "1", instrumentalness: "0" },
 		description: "Chill {{type}} for a beach bonfire",
 	},
 	{
-		traits: {energy: "1", acousticness: "0", valence: "1", speechiness: "0"},
+		traits: { energy: "1", acousticness: "0", valence: "1", speechiness: "0" },
 		description: "Melodic {{type}} for a sing-along",
 	},
 	{
-		traits: {danceability: "1", energy: "1", valence: "1", speechiness: "0"},
+		traits: { danceability: "1", energy: "1", valence: "1", speechiness: "0" },
 		description: "Funkalicious {{type}} for a funky good time",
 	},
 	{
-		traits: {danceability: "1", energy: "1", valence: "1", acousticness: "0", instrumentalness: "0"},
+		traits: { danceability: "1", energy: "1", valence: "1", acousticness: "0", instrumentalness: "0" },
 		description: "Old-school {{type}} for a throwback party",
 	},
 	{
-		traits: {energy: "0", acousticness: "0", valence: "0", instrumentalness: "1", speechiness: "0"},
+		traits: { energy: "0", acousticness: "0", valence: "0", instrumentalness: "1", speechiness: "0" },
 		description: "Dreamy {{type}} for a dreamy vibe",
 	},
 	{
-		traits: {danceability: "1", energy: "1"},
+		traits: { danceability: "1", energy: "1" },
 		description: "Danceable {{type}} for getting your groove on",
 	},
 	{
-		traits: {acousticness: "0", valence: "0"},
+		traits: { acousticness: "0", valence: "0" },
 		description: "Mellow {{type}} for a laid-back evening",
 	},
 	{
-		traits: {energy: "1", valence: "1"},
+		traits: { energy: "1", valence: "1" },
 		description: "Rockin' {{type}} for a rock concert",
 	},
 	{
-		traits: {danceability: "0", valence: "0"},
+		traits: { danceability: "0", valence: "0" },
 		description: "Smooth {{type}} for a romantic date",
 	},
 	{
-		traits: {energy: "1", danceability: "1"},
+		traits: { energy: "1", danceability: "1" },
 		description: "Punchy {{type}} for a workout",
 	},
 	{
-		traits: {instrumentalness: "0", valence: "0"},
+		traits: { instrumentalness: "0", valence: "0" },
 		description: "Mystical {{type}} for a meditative mood",
 	},
 	{
-		traits: {energy: "1", danceability: "1"},
+		traits: { energy: "1", danceability: "1" },
 		description: "Electrifying {{type}} for a dance party",
 	},
 	{
-		traits: {instrumentalness: "0", speechiness: "1"},
+		traits: { instrumentalness: "0", speechiness: "1" },
 		description: "Intriguing {{type}} for a thought-provoking playlist",
 	},
 	{
-		traits: {energy: "1", valence: "1"},
+		traits: { energy: "1", valence: "1" },
 		description: "Sassy {{type}} for a girls' night out",
 	},
 ] as {
-	traits: {[key in keyof typeof FEATURES]?: keyof typeof FEATURES[key]}
+	traits: { [key in keyof typeof FEATURES]?: keyof typeof FEATURES[key] }
 	description: string,
 	ignore?: (keyof typeof FEATURES)[]
 }[]
