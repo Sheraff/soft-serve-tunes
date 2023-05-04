353b18f390491a96366b043dce31e429fe34dee9
Sheraff <fpellet@ensc.fr>
2023-05-04 14:15:35 +0200

    allow origin at /api/local/ping

--- src/pages/api/local/ping.ts
+++ src/pages/api/local/ping.ts
@@ -3,11 +3,11 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import { getLocalNetworkAuth } from "server/common/local-auth"
 
 export default async function local (req: NextApiRequest, res: NextApiResponse) {
+	res.setHeader("Access-Control-Allow-Origin", env.NEXT_PUBLIC_INTERNET_HOST)
 	const isLocal = getLocalNetworkAuth(req)
 	if (!isLocal) {
 		res.status(401).json({ error: "reserved for intranet access", ip: req.socket.remoteAddress })
 		return
 	}
-	res.setHeader("Access-Control-Allow-Origin", env.NEXT_PUBLIC_INTERNET_HOST)
 	res.status(204).end()
 }
